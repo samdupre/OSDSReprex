a2bfd9d77a9444f7570cc50b8a895b1ca33f7f33
samdupre <77623569+samdupre@users.noreply.github.com>
2021-12-02 09:11:45 -0500

    Added components.R

---
+++ components.R
@@ -0,0 +1,28 @@
+makeCard <- function(title, content, size = 12, style = "") {
+  div(
+    class = glue("card ms-depth-8 ms-sm{size} ms-xl{size}"),
+    style = style,
+    Stack(
+      tokens = list(childrenGap = 5),
+      Text(variant = "large", title, block = TRUE),
+      content
+    )
+  )  
+}
+
+makePage <- function (title, subtitle, contents) {
+  tagList(div(
+    class = "page-title",
+    span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style =
+           "color: #323130"),
+    tags$br(),
+    span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style =
+           "color: #605E5C; margin: 14px;")
+  ),
+  contents)
+}
+
+gadm_shapefile_pull <- function(your_country, level_needed) {
+  raster::getData("GADM", country = your_country, level = level_needed)
+}
+
